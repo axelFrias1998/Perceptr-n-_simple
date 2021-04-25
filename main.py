2e27082559ea455cf25bacd028f9bd4b0f30f429
Axel Frías Hernández <axel.frias257@gmail.com>
2021-04-25 18:20:28 -0500

    Cálculos y .gitignore con carpeta .vscode/

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+lambda = factor de aprendizaje = '.2
+
+Cálculo del error = e = (y - z)
+
+e permite ajustar cambio de umbral (theta) = DELTA THETA = -(LAMBDA * e)
+
+Wi se ajustan sumándoles = DELTA Wi = LAMBDA * e * Xi
+'''
